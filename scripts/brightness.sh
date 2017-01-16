8b9c45b4b711e2145df0787c8a99880ccf0dd6ee
Shivang Saxena <shivangs44@gmail.com>
2017-01-16 13:10:46 -0500

    Added more config files

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Shivang Saxena
 # A very unintelligent script to lower/raise brightness.
 # First argument must be a '+' or '-' 
 
@@ -7,12 +6,12 @@ direction=$1
 max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
 current_brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
 
-if [ $direction = "+" ]
-then
-    new_brightness=$(printf %.0f $(echo "$current_brightness + 50" | bc -l))
-    $(echo "$new_brightness" >> /sys/class/backlight/intel_backlight/brightness) 
-else 
-    new_brightness=$(printf %.0f $(echo "$current_brightness - 50" | bc -l))
-    $(echo "$new_brightness" >> /sys/class/backlight/intel_backlight/brightness) 
-fi
+    if [ $direction = "+" ]
+    then
+        new_brightness=$(printf %.0f $(echo "$current_brightness + 50" | bc -l))
+        $(echo "$new_brightness" >> /sys/class/backlight/intel_backlight/brightness) 
+    else 
+        new_brightness=$(printf %.0f $(echo "$current_brightness - 50" | bc -l))
+        $(echo "$new_brightness" >> /sys/class/backlight/intel_backlight/brightness) 
+    fi
 
